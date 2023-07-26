ab10caaeb545ac48eb9409c261c066f5b3dab9c8
andybi7676 <andybi7676@gmail.com>
2023-07-26 21:49:29 +0800

    init wtg

---
+++ wtg-app/App.js
@@ -0,0 +1,24 @@
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <MapView style={styles.map} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
