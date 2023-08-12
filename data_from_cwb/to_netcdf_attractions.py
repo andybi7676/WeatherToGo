312370a6947618515ff9457b5b142d6e699c8fda
MNinng <113407315+MNinng@users.noreply.github.com>
2023-08-12 13:06:49 +0800

    Update to_netcdf_attractions.py
    Automatically fetch data from cwb at 01L, 07L, 13L, 19L

--- data_from_cwb/to_netcdf_attractions.py
+++ data_from_cwb/to_netcdf_attractions.py
@@ -3,26 +3,36 @@ import pandas as pd
 import numpy as np
 import xarray as xr
 import requests
+import datetime
+import threading
 
+
+# CONSTANT
 dataset = ['003', '005', '009', '011', '015', '017', '021', '023', '027', '029', '031', '035', '039', '041', '045', '047', '051', '053', '057', '059', '063', '065', '069', '071', '076', '078', '082', '084']
+FETCH_CWB_RATE = 6 # 6hr: 00, 06,12,18L, update after 1 hr. -> 01, 07, 13, 19
 
-for i in range(len(dataset)):
-    url = 'https://opendata.cwb.gov.tw/fileapi/v1/opendataapi/F-B0053-'+str(dataset[i])+'?Authorization=rdec-key-123-45678-011121314&format=JSON'
-    response = requests.get(url)
-    data = response.json()
-    locations = data["cwbopendata"]["dataset"]["locations"]["location"]
 
-    data_list = []
-    for locationname in locations:
-        location_name = locationname["locationName"]
-        lon = locationname["lon"]
-        lat = locationname["lat"]
-        weatherelement = locationname["weatherElement"]
-        for element in weatherelement:
-            elementName = element["elementName"]
-            description = element["description"]
-            time = element["time"]
-            #for time_info in time:
+# fetch_cwb() is the same as original to_netcdf_attractoins.py
+def fetch_cwb(isLoop=False):
+    start = datetime.datetime.now()
+    print("   Start fetch cwb at:", start)
+    for i in range(len(dataset)):
+        url = 'https://opendata.cwb.gov.tw/fileapi/v1/opendataapi/F-B0053-'+str(dataset[i])+'?Authorization=rdec-key-123-45678-011121314&format=JSON'
+        response = requests.get(url)
+        data = response.json()
+        locations = data["cwbopendata"]["dataset"]["locations"]["location"]
+
+        data_list = []
+        for locationname in locations:
+            location_name = locationname["locationName"]
+            lon = locationname["lon"]
+            lat = locationname["lat"]
+            weatherelement = locationname["weatherElement"]
+            for element in weatherelement:
+                elementName = element["elementName"]
+                description = element["description"]
+                time = element["time"]
+                #for time_info in time:
                 if elementName=="T" or elementName=="AT" or elementName=="Td" or elementName=="RH" or elementName=="MaxT" or elementName=="MinT" or elementName=="MaxAT" or elementName=="MinAT" or elementName=="PoP6h" or elementName=="PoP12h" or elementName=="PoP24h" or elementName=="WD" or elementName=="WeatherDescription":
                     for time_info in time:
                         start_time = time_info.get('startTime')
@@ -37,15 +47,14 @@ for i in range(len(dataset)):
                             "lon": lon,
                             "lat": lat,
                             "ElementName": elementName,
-                            "description": description, 
+                            "description": description,
                             "StartTime": start_time,
                             "EndTime": end_time,
                             "DataTime": data_time,
                             "Value": values,
                             "Measures": measures
                         })
-                        if elementName=='RH':
-                            print('RH')
+
                 else:
                     for time_info in time:
                         start_time = time_info.get('startTime')
@@ -62,14 +71,56 @@ for i in range(len(dataset)):
                                 "lon": lon,
                                 "lat": lat,
                                 "ElementName": elementName,
-                                "description": description, 
+                                "description": description,
                                 "StartTime": start_time,
                                 "EndTime": end_time,
                                 "DataTime": data_time,
                                 "Value": values,
                                 "Measures": measures
                             })
+        ds = xr.Dataset.from_dataframe(pd.DataFrame(data_list))
+        ds.to_netcdf("weather_data_attractions_"+str(dataset[i])+".nc")
+        print("   | weather_data_attractions_"+str(dataset[i])+".nc")
 
-    ds = xr.Dataset.from_dataframe(pd.DataFrame(data_list))
-    ds.to_netcdf("weather_data_attractions_"+str(dataset[i])+".nc")
-    print("weather_data_attractions_"+str(dataset[i])+".nc")
+    # print total fetch time
+    end = datetime.datetime.now()
+    print('   Total Cost time:', format(end-start))
+
+    # Fetch Loop
+    if isLoop:
+        now_time, next_time, timedelta_next_fetch = get_next_time()
+        print('== Next fetch Time :', next_time)
+        loop_fetch_cwb(timedelta_next_fetch)
+
+
+def loop_fetch_cwb(time_delta):
+    timer = threading.Timer(time_delta, fetch_cwb, (True,))
+    timer.start()
+
+
+def get_next_time():
+    now_time = datetime.datetime.now()
+
+    # Find the next [01, 07, 13, 19] hr
+    delta_hour = (FETCH_CWB_RATE - (now_time.hour - 1) % FETCH_CWB_RATE)
+    next_time = datetime.datetime.strptime(
+        str(now_time.year) + "-" + str(now_time.month) + "-" + str(now_time.day) + ' '
+        + str(now_time.hour) + ':00:00', "%Y-%m-%d %H:%M:%S") + datetime.timedelta(hours=delta_hour)
+
+    # Count the interval
+    timedelta_next_fetch = (next_time - now_time).total_seconds()
+
+    return now_time, next_time, timedelta_next_fetch
+
+
+if __name__ == '__main__':
+    # Find the next [01, 07, 13, 19] hr
+    now_time, next_time, timedelta_second_fetch = get_next_time()
+
+    # No matter when, fetch at first.
+    fetch_cwb()
+
+    # Fetch Loop
+    print('== First fetch Time :', now_time)
+    print('== Next fetch Time :', next_time)
+    loop_fetch_cwb(timedelta_second_fetch)
